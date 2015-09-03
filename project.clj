1484cd87d27b279e08dc918055dfdcddebba3d01
Brad Lucas <brad@beaconhill.com>
2015-09-03 14:18:51 -0400

    updated version numbers

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cmdline "1.0.0"
   :description "Command line application in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main cmdline.core
   )
